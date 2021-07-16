cec383a866bf110cbf05d57554cb53aa04721462
unme88 <sunny.on.a.roll@gmail.com>
2021-07-16 11:30:23 +0900

    feat: dummy Data 추가

--- ProjectManager/ProjectManager/DummyData.swift
+++ ProjectManager/ProjectManager/DummyData.swift
@@ -7,4 +7,22 @@
 
 import Foundation
 
+// MARK: - todoDummy
+var todo: [Memo] = [
+    Memo(title: "써니는 공부중", content: "드래그앤드랍 끝내쟈 제발", date: "2021.07.16"),
+    Memo(title: "코딩 공부", content: "문법 공부 그만! 구현하자 💻", date: "2021.07.16")
+]
+
+// MARK: - doingDummy
+var doing: [Memo] = [
+    Memo(title: "점심 뭐먹지", content: "너무 덥다. 힘들 내쟈 💪", date: "2021.07.17"),
+    Memo(title: "나는 할 수 있따!!", content: "아쟈아쟈", date: "2021.07.17")
+]
+
+// MARK: - doneDummy
+var done: [Memo] = [
+    Memo(title: "짧은 메모", content: "내용 없음", date: "2021.07.16"),
+    Memo(title: "짧은 메모", content: "내용 없음", date: "2021.07.16")
+]
+
 
